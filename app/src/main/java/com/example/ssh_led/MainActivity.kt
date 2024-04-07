fdfec9138dd040a1fce43995d8e0ba53b5e241a0
wldnr <wldnror@naver.com>
2024-04-07 21:37:40 +0900

    2차

--- app/src/main/java/com/example/ssh_led/MainActivity.kt
+++ app/src/main/java/com/example/ssh_led/MainActivity.kt
@@ -1,20 +1,54 @@
 package com.example.ssh_led
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import java.io.IOException
+import java.net.DatagramPacket
+import java.net.DatagramSocket
+import java.net.InetAddress
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var ipAddressTextView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val button: Button = findViewById(R.id.sendButton)
+        ipAddressTextView = findViewById(R.id.ipAddressTextView)
+
+        button.setOnClickListener {
+            sendUDPBroadcast("Hello Raspberry Pi!", 12345)
         }
+    }
+
+    private fun sendUDPBroadcast(message: String, port: Int) {
+        Thread(Runnable {
+            try {
+                val socket = DatagramSocket()
+                socket.broadcast = true
+                val sendData = message.toByteArray()
+                val sendPacket = DatagramPacket(
+                    sendData,
+                    sendData.size,
+                    InetAddress.getByName("255.255.255.255"),
+                    port
+                )
+                socket.send(sendPacket)
+
+                // 라즈베리 파이의 IP 주소를 표시
+                val raspPiAddress = sendPacket.address.hostAddress
+                runOnUiThread {
+                    ipAddressTextView.text = "라즈베리 파이 IP 주소: $raspPiAddress"
+                }
+
+                socket.close()
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+        }).start()
     }
 }
