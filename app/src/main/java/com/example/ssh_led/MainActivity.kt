3e67b445d455f62afe78655bb7d20ed2d730a081
jiyongjun <wldnror@naver.com>
2024-05-01 13:49:08 +0900

    버튼추가

--- app/src/main/java/com/example/ssh_led/MainActivity.kt
+++ app/src/main/java/com/example/ssh_led/MainActivity.kt
@@ -5,17 +5,20 @@ import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import android.os.Bundle
+import android.widget.Button
+import android.widget.GridLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.localbroadcastmanager.content.LocalBroadcastManager
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var ipAddressTextView: TextView // 이 부분이 TextView의 참조를 제대로 선언합니다.
+    private lateinit var ipAddressTextView: TextView
+    private lateinit var buttonGrid: GridLayout
 
     private val ipReceiver = object : BroadcastReceiver() {
         override fun onReceive(context: Context?, intent: Intent?) {
             val ip = intent?.getStringExtra("ip_address")
-            ipAddressTextView.text = "라즈베리 파이 IP 주소: $ip"
+            ipAddressTextView.text = "AI멀티백 IP 주소: $ip"
         }
     }
 
@@ -23,11 +26,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        ipAddressTextView = findViewById(R.id.ipAddressTextView)  // 여기에서 TextView를 찾아서 초기화합니다.
+        ipAddressTextView = findViewById(R.id.ipAddressTextView)
+        buttonGrid = findViewById(R.id.buttonGrid)
 
         LocalBroadcastManager.getInstance(this).registerReceiver(
             ipReceiver, IntentFilter("UPDATE_IP_ADDRESS")
         )
+
+        // 버튼 동적으로 생성 및 이벤트 처리
+        for (i in 1..12) {
+            val button = Button(this).apply {
+                text = "버튼 $i"
+                setOnClickListener { sendSignal(i.toString().padStart(4, '0')) }
+            }
+            buttonGrid.addView(button)
+        }
 
         Intent(this, NetworkScanService::class.java).also {
             startForegroundService(it)
@@ -37,5 +50,11 @@ class MainActivity : AppCompatActivity() {
     override fun onDestroy() {
         super.onDestroy()
         LocalBroadcastManager.getInstance(this).unregisterReceiver(ipReceiver)
+    }
+
+    private fun sendSignal(signal: String) {
+        val intent = Intent(this, NetworkScanService::class.java)
+        intent.putExtra("signal", signal)
+        startService(intent)
     }
 }
