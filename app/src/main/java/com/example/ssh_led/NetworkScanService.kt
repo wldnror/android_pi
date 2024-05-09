5e37b3ff442b9a946c27d413ec82a0f38d73900c
jiyongjun <wldnror@naver.com>
2024-05-09 23:59:01 +0900

    푸쉬 알림 버그 수정 1

--- app/src/main/java/com/example/ssh_led/NetworkScanService.kt
+++ app/src/main/java/com/example/ssh_led/NetworkScanService.kt
@@ -10,27 +10,27 @@ import android.os.Build
 import android.os.Handler
 import android.os.HandlerThread
 import android.os.IBinder
-import android.util.Log
-import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import java.io.IOException
 import java.net.DatagramPacket
 import java.net.DatagramSocket
 import java.net.InetAddress
+import android.util.Log
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import java.util.Timer
 import java.util.TimerTask
 
 class NetworkScanService : Service() {
     private val udpPort = 12345
-    private lateinit var handlerThread: HandlerThread
+    private val handlerThread = HandlerThread("NetworkThread")
     private lateinit var handler: Handler
     private var lastIpNotification: String? = null
-    private var lastUpdateTime: Long = 0
+    private var lastUpdateTime: Long = 0L
     private var timer: Timer? = null
     private var isDisconnected = false
 
     override fun onCreate() {
         super.onCreate()
-        handlerThread = HandlerThread("NetworkThread").also { it.start() }
+        handlerThread.start()
         handler = Handler(handlerThread.looper)
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val channel = NotificationChannel(
@@ -38,20 +38,26 @@ class NetworkScanService : Service() {
                 "Service Channel",
                 NotificationManager.IMPORTANCE_DEFAULT
             )
-            getSystemService(NotificationManager::class.java)?.createNotificationChannel(channel)
+            getSystemService(NotificationManager::class.java).createNotificationChannel(channel)
         }
     }
 
-    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        val userAction = intent?.getBooleanExtra("userAction", false) ?: false
+    override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
+        val userAction = intent.getBooleanExtra("userAction", false)
         if (!userAction) {
             startForegroundServiceWithNotification()
         }
-        val signal = intent?.getStringExtra("signal") ?: "REQUEST_IP"
+        val signal = intent.getStringExtra("signal") ?: "REQUEST_IP"
 
         Thread {
             when (signal) {
-                "Right Blinker Activated", "Left Blinker Activated", "REQUEST_RECORDING_STATUS" -> {
+                "Right Blinker Activated", "Left Blinker Activated" -> {
+                    sendSignal(signal)
+                    if (!userAction) {
+                        resetTimer()
+                    }
+                }
+                "REQUEST_RECORDING_STATUS" -> {
                     sendSignal(signal)
                     if (!userAction) {
                         resetTimer()
@@ -71,6 +77,7 @@ class NetworkScanService : Service() {
             startSignalSending()
             listenForUdpBroadcast()
         }
+
         return START_NOT_STICKY
     }
 
@@ -118,17 +125,16 @@ class NetworkScanService : Service() {
     private fun listenForUdpBroadcast() {
         Thread {
             try {
-                DatagramSocket(null).apply {
+                val socket = DatagramSocket(null).apply {
                     reuseAddress = true
                     bind(java.net.InetSocketAddress(udpPort))
-                }.use { socket ->
-                    val buffer = ByteArray(1024)
-                    while (true) {
-                        val packet = DatagramPacket(buffer, buffer.size)
-                        socket.receive(packet)
-                        resetTimer()
-                        handleReceivedPacket(packet)
-                    }
+                }
+                val buffer = ByteArray(1024)
+                while (true) {
+                    val packet = DatagramPacket(buffer, buffer.size)
+                    socket.receive(packet)
+                    resetTimer()
+                    handleReceivedPacket(packet)
                 }
             } catch (e: IOException) {
                 e.printStackTrace()
@@ -139,17 +145,25 @@ class NetworkScanService : Service() {
 
     private fun resetTimer() {
         synchronized(this) {
-            timer?.cancel()
-            timer = Timer().apply {
-                scheduleAtFixedRate(object : TimerTask() {
-                    override fun run() {
-                        if (System.currentTimeMillis() - lastUpdateTime >= 5000) {
-                            isDisconnected = true
-                            updateConnectionStatus(false)
-                        }
-                    }
-                }, 2000, 2000) // 10초 후에 작업 실행, 10초마다 반복
+            // 기존 타이머 취소
+            try {
+                timer?.cancel()
+                timer?.purge()
+            } catch (e: IllegalStateException) {
+                Log.e("NetworkScanService", "Timer cancel error: ", e)
             }
+        }
+        // 새로운 타이머 생성
+        timer = Timer().apply {
+            schedule(object : TimerTask() {
+                override fun run() {
+                    // 타이머가 만료될 때만 isDisconnected를 true로 설정
+                    if (System.currentTimeMillis() - lastUpdateTime >= 5000) {
+                        isDisconnected = true
+                        updateConnectionStatus(false)
+                    }
+                }
+            }, 10000) // 5초 후에 작업 실행
         }
     }
 
@@ -159,14 +173,16 @@ class NetworkScanService : Service() {
         val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
         if (isConnected && !lastKnownIP.isNullOrEmpty()) {
+            // 연결된 IP로 알림 업데이트
             updateNotification(lastKnownIP)
         } else if (!isConnected && lastIpNotification != "DISCONNECTED") {
+            // 연결 끊김 알림 업데이트 (연결 끊김 상태가 변경되었을 때만)
             val notificationBuilder = Notification.Builder(this, "service_channel").apply {
                 setContentTitle("용굴라이더와 연결되지 않았습니다.")
                 setSmallIcon(android.R.drawable.stat_notify_sync)
             }
             startForeground(1, notificationBuilder.build())
-            lastIpNotification = "DISCONNECTED"
+            lastIpNotification = "DISCONNECTED"  // 상태 업데이트
         }
     }
 
@@ -178,6 +194,8 @@ class NetworkScanService : Service() {
             val ipInfo = parts[0].substring(3).trim()
             val status = parts[1].trim()
             val prefs = getSharedPreferences("NetworkPreferences", Context.MODE_PRIVATE)
+
+            // IP 정보를 저장하고 관련 알림을 업데이트합니다.
             prefs.edit().putString("last_ip_address", ipInfo).apply()
             val ipIntent = Intent("UPDATE_IP_ADDRESS").apply { putExtra("ip_address", ipInfo) }
             LocalBroadcastManager.getInstance(this).sendBroadcast(ipIntent)
